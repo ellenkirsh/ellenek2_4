7c5ffe001a163b9d94f23135e4c1d63074cfd1b8
ellenkirsh <ellenek2@illinois.edu>
2021-09-19 21:16:24 -0500

    Update main.cpp

--- ellenek2_4/main.cpp
+++ ellenek2_4/main.cpp
@@ -6,9 +6,21 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(void) {
+    int number, digit;
+    int sum = 0;
+    int prod = 1;
+    cout << "Enter an integer: ";
+    cin >> number;
+    do {
+        digit = number % 10;
+        number = number/10;
+        sum += digit;
+        prod *= digit;
+    }
+    while (number > 0);
+    cout << "Sum is " << sum << " and Product is "<< prod<< endl;
     return 0;
 }
